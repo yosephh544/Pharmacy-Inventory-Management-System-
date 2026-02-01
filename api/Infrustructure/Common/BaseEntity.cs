453459425f425e402e43b20ca3487ff66853ff2f
yosephh99 <yosephhaile99@gmail.com>
2026-02-01 20:30:33 +0300

    feat(api): add BaseEntity and PharmacyProfiles classes for inventory management

---
+++ api/Infrustructure/Common/BaseEntity.cs
@@ -0,0 +1,10 @@
+namespace Pharmacy.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public int? CreatedBy { get; set; }
+        public bool IsDeleted { get; set; } = false;
+    }
+}
